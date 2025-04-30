8bd72e95747a154faa73f09ee6ffa5621eb28f63
Cristian Quispe Llanos <quispellanoscristian883@gmail.com>
2025-04-30 20:39:46 -0300

    subiendo ejemplo_1 y ejemplo_3

---
+++ 03 Interfaz/Grupo 1/ejemplo_1_interfaz/pajaro.java
@@ -0,0 +1,10 @@
+package ejemplo_1_interfaz;
+
+public class pajaro implements volador {
+
+    @Override
+    public void volar() {
+        System.out.println("El pájaro está volando.");
+    }
+
+}
